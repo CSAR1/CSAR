8e42bb1b3c4a77b1cb55936a53dca5b2ea32460a
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-18 16:44:47 +0800

    设置A-10动态贴图

---
+++ CSAR Simulation/Assets/Scripts/Model/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+    private Transform m_Transform;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Transform = gameObject.GetComponent<Transform>();
+        //speed = 0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Transform.Rotate(Vector3.forward * speed, Space.Self);
+    }
+}
