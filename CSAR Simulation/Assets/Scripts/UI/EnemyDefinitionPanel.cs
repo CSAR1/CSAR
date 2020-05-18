67e7e3dc04f34dcab5c24f780ad1330664cc63b8
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-18 12:20:42 +0800

    建立敌方想定界面

---
+++ CSAR Simulation/Assets/Scripts/UI/EnemyDefinitionPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UIFramework;
+public class EnemyDefinitionPanel : BasePanel
+{
+    private CanvasGroup canvasGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (canvasGroup == null)
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+    }
+
+    public override void OnEnter()
+    {
+        if (canvasGroup == null) canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+
+        transform.localScale = Vector3.zero;
+        transform.DOScale(1, .5f);
+    }
+
+    public override void OnExit()
+    {
+        //canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+
+        transform.DOScale(0, .5f).OnComplete(() => canvasGroup.alpha = 0);
+    }
+
+    public void OnClosePanel()
+    {
+        UIManager.Instance.PopPanel();
+    }
+}
