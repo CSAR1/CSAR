e2bd57cffdaf195d97130c377c21ce531ef285f4
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-20 10:41:47 +0800

    添加全局变量命名空间

---
+++ CSAR Simulation/Assets/Scripts/Global/GlobalParameters.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalParameters
+{
+    static class TaskDefinition
+    {
+
+    }
+
+    static class EnemyDefinition
+    {
+
+    }
+
+    static class A_10
+    {
+
+    }
+
+    static class MH_53
+    {
+
+    }
+
+    static class AC_130
+    {
+
+    }
+
+    static class MH_60
+    {
+
+    }
+}
