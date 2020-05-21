6f80ef4b811152ec9b0efe92365b18b3cb790225
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-21 11:07:09 +0800

    完成任务想定的参数更新逻辑

--- CSAR Simulation/Assets/Scripts/UI/TaskDefinitionPanel.cs
+++ CSAR Simulation/Assets/Scripts/UI/TaskDefinitionPanel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UIFramework;
+using UnityEngine.UI;
+using GlobalParameters;
 
 public class TaskDefinitionPanel : BasePanel
 {
     private CanvasGroup canvasGroup;
+    
+    public Text pilotNum; //遇险人数
+    public Text lifeLeft; //剩余生命
+    public Dropdown hideEnemyCap; //躲避敌方的能力
+    public Text maxSpeed; //最大移动速度
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +22,7 @@ public class TaskDefinitionPanel : BasePanel
         {
             canvasGroup = GetComponent<CanvasGroup>();
         }
+        hideEnemyCap.onValueChanged.AddListener(OnHideEnemyCapEndEdit);
     }
 
     public override void OnEnter()
@@ -38,5 +46,52 @@ public class TaskDefinitionPanel : BasePanel
     public void OnClosePanel()
     {
         UIManager.Instance.PopPanel();
+    }
+
+    public void OnPilotNumEndEdit(string s)
+    {
+        string content = pilotNum.text;
+        //Debug.Log("Before = " + TaskDefinition.pilotNum);
+        if (int.TryParse(content, out int data))
+        {
+            TaskDefinition.pilotNum = data;
+        }
+        //Debug.Log("After = " + TaskDefinition.pilotNum);
+    }
+
+    public void OnLifeLeftEndEdit(string s)
+    {
+        string content = lifeLeft.text;
+        if (int.TryParse(content, out int data))
+        {
+            TaskDefinition.lifeLeft = data;
+        }
+    }
+
+    public void OnHideEnemyCapEndEdit(int n)
+    {
+        //Debug.Log(TaskDefinition.hideEnemyCap.ToString());
+        switch (n)
+        {
+            case 0:
+                TaskDefinition.hideEnemyCap = HideEnemyCap.high;
+                break;
+            case 1:
+                TaskDefinition.hideEnemyCap = HideEnemyCap.medium;
+                break;
+            case 2:
+                TaskDefinition.hideEnemyCap = HideEnemyCap.low;
+                break;
+        }
+        //Debug.Log(TaskDefinition.hideEnemyCap.ToString());
+    }
+
+    public void OnMaxSpeedEndEdit(string s)
+    {
+        string content = maxSpeed.text;
+        if (int.TryParse(content, out int data))
+        {
+            TaskDefinition.maxSpeed = data;
+        }
     }
 }
