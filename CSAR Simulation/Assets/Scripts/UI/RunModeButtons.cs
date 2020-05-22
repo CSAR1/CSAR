a20a1d8a3cd65ae6b3fdb2f15f83d15e4f1e2581
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-22 19:55:38 +0800

    暂时保存版本

---
+++ CSAR Simulation/Assets/Scripts/UI/RunModeButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UIFramework;
+
+public class RunModeButtons : MonoBehaviour
+{
+    public void OnPushPanel(string panelTypeString)
+    {
+        UIPanelType panelType = (UIPanelType)System.Enum.Parse(typeof(UIPanelType), panelTypeString);
+        UIManager.Instance.PushPanel(panelType);
+    }
+
+    public void OnTest()
+    {
+        OnPushPanel("Warning");
+    }
+
+    public void OnQuitPressed(string headline, string content)
+    {
+        //OnPushPanel("WarningPanel");
+    }
+}
--- CSAR Simulation/Assets/Scripts/UI/WarningPanel.cs
+++ CSAR Simulation/Assets/Scripts/UI/WarningPanel.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UIFramework;
+using DG.Tweening;
 
 public class WarningPanel : BasePanel
 {
+    private CanvasGroup canvasGroup;
+    private GameObject runModeButtons;
+    private GameObject mainMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (canvasGroup == null)
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+        runModeButtons = GameObject.Find("RunModeButtons(Clone)");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEnter()
     {
-        
+        if (canvasGroup == null) canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+
+        transform.localScale = Vector3.zero;
+        transform.DOScale(1, .5f);
+    }
+
+    public override void OnExit()
+    {
+        //canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+
+        transform.DOScale(0, .5f).OnComplete(() => canvasGroup.alpha = 0);
+    }
+
+    public void OnClosePanel()
+    {
+        UIManager.Instance.PopPanel();
+    }
+
+    public void OnQuitConfirm()
+    {
+        UIManager.Instance.PopPanel();
+        runModeButtons.SetActive(false);
+        //mainMenu.SetActive(true);
     }
 }
