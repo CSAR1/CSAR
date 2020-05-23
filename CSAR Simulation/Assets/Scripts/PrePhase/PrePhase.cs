964e7372dc59c200a5391557407ec8963ae6090d
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-23 20:25:30 +0800

    添加主界面UI

---
+++ CSAR Simulation/Assets/Scripts/PrePhase/PrePhase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrePhase : MonoBehaviour
+{
+    //private float
+
+    void Start()
+    {
+        
+    }
+    
+    void FixedUpdate()
+    {
+        
+    }
+}
