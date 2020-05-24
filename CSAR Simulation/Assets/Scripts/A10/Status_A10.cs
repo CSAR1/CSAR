f9715663d07ad6f7bafe54097f829027a6b6ceb9
Several_Miles <38178440+SeveralMiles@users.noreply.github.com>
2020-05-24 22:22:04 +0800

    调整A10脚本位置

--- CSAR Simulation/Assets/Scripts/A10/Status_A10.cs
+++ CSAR Simulation/Assets/Scripts/A10/Status_A10.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GlobalParameters;
 
 public class Status_A10 : MonoBehaviour
 {
@@ -14,5 +15,17 @@ public class Status_A10 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Fixedupdate()
+    {
+        if (SimulationRun.runMode == RunMode.run)
+        {
+            // if (发现状态)｛｝
+            // 搜索 
+            // 掩护
+            // 撤离
+        }
+
     }
 }
