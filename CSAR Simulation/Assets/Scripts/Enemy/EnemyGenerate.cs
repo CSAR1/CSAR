bd87c22e816fed2ddb3dd59663cac086d5059518
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-24 12:08:33 +0800

    初始化敌方生成

--- CSAR Simulation/Assets/Scripts/Enemy/EnemyGenerate.cs
+++ CSAR Simulation/Assets/Scripts/Enemy/EnemyGenerate.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GlobalParameters;
+using UIFramework;
 
 public class EnemyGenerate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int enemiesNum;
+    private int missileRange;
+    private int missileMach;
+    private int maxOverload;
+    private int detectR;
+    private MainMenu mainMenu;
+
     void Start()
+    {
+        mainMenu = UIManager.Instance.GetPanel(UIPanelType.MainMenu) as MainMenu;
+        mainMenu.OnStart += InitValue;
+    }
+    
+    void FIxedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitValue()
     {
-        
+        enemiesNum = EnemyDefinition.enemyNum;
+        missileRange = EnemyDefinition.missileRange;
+        missileMach = EnemyDefinition.missileMach;
+        maxOverload = EnemyDefinition.maxOverload;
+        detectR = EnemyDefinition.detectR;
     }
 }
