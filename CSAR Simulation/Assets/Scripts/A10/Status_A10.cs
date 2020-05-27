756d890c3cc9841ec96ae7edf9439f79be4c9281
Several_Miles <38178440+SeveralMiles@users.noreply.github.com>
2020-05-27 23:30:10 +0800

    将A10部署至场景

--- CSAR Simulation/Assets/Scripts/A10/Status_A10.cs
+++ CSAR Simulation/Assets/Scripts/A10/Status_A10.cs
@@ -117,13 +117,6 @@ public class Status_A10 : MonoBehaviour
                     // 发现，掩护函数
                     CoverA10();
                     break;
-<<<<<<< HEAD
-
-=======
-                //case PilotDetectedMode.recovered:
-                    // 救援，撤离护航函数
-                    //break;
->>>>>>> 373e9521945196f3f8f391ef8fe51f5178339be6
             }
         }
 
