6fdeb1544cd20254e01de90f61223d9e44e3b89b
JxBalance <34788672+JxBalance@users.noreply.github.com>
2020-05-28 20:46:41 +0800

    控制EC130复原

--- CSAR Simulation/Assets/Scripts/AWACS/EC_130.cs
+++ CSAR Simulation/Assets/Scripts/AWACS/EC_130.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GlobalParameters;
+using UIFramework;
 
 public class EC_130 : MonoBehaviour
 {
+    private MainMenu mainMenu;
 
     private float angle;
     public float r = 200;
@@ -16,19 +19,29 @@ public class EC_130 : MonoBehaviour
     void Start()
     {
         sun = GameObject.Find("Sun");
-        angle = 0f;
-        gameObject.GetComponent<Transform>().position = new Vector3(0.3f, 0.7f, 1f);  //重置做圆周的开始位置
-        r = Vector3.Distance(gameObject.GetComponent<Transform>().position, sun.GetComponent<Transform>().position); //两个物品间的距离
+        mainMenu = UIManager.Instance.GetPanel(UIPanelType.MainMenu) as MainMenu;
+        mainMenu.OnStart += OnStart;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        w = 0.065f;
-        angle -= w * Time.deltaTime;
-        x = Mathf.Cos(angle) * r;
-        z = -Mathf.Sin(angle) * r;
-        transform.position = new Vector3(sun.GetComponent<Transform>().position.x - x, 0.7f, sun.GetComponent<Transform>().position.z - z);
-        gameObject.transform.Rotate(-0.065f * Vector3.up);
+        if (SimulationRun.runMode == RunMode.run)
+        {
+            w = 0.065f;
+            angle -= w * Time.deltaTime;
+            x = Mathf.Cos(angle) * r;
+            z = -Mathf.Sin(angle) * r;
+            transform.position = new Vector3(sun.GetComponent<Transform>().position.x - x, 0.7f, sun.GetComponent<Transform>().position.z - z);
+            gameObject.transform.Rotate(-0.065f * Vector3.up);
+        }
+    }
+
+    void OnStart()
+    {
+        angle = 0f;
+        gameObject.transform.position = new Vector3(0.3f, 0.7f, 1f);  //重置做圆周的开始位置
+        gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
+        r = Vector3.Distance(gameObject.GetComponent<Transform>().position, sun.GetComponent<Transform>().position); //两个物品间的距离
     }
 }
