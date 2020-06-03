b56fbd7382fb8abc8df614a2818e8caff135db71
liyuge12 <48047926+liyuge12@users.noreply.github.com>
2020-06-03 15:19:02 +0800

    直升机模型替换

---
+++ CSAR Simulation/Assets/Scripts/Helicopter/helicopterModel/RotorWingX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GlobalParameters;
+using UIFramework;
+
+public class RotorWingX : MonoBehaviour
+{
+    private float speed_rotor = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (SimulationRun.runMode == RunMode.run)
+        {
+            this.transform.Rotate(new Vector3(speed_rotor, 0, 0));//螺旋桨旋转
+
+        }
+    }
+
+}
